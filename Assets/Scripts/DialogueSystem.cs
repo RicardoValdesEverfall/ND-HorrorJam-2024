48c95e77baca157ea0106c9382e9f5a9cd6d053d
RicardoValdesEverfall <72148793+RicardoValdesEverfall@users.noreply.github.com>
2024-05-14 14:42:34 -0400

    UI choice animations

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DialogueSystem : MonoBehaviour
 {
     [SerializeField] GameObject Camille;
+    [SerializeField] GameObject PlayerCamera;
 
     private Animator director;
     public CanvasGroup[] canvasGroup;
@@ -21,17 +22,34 @@ public class DialogueSystem : MonoBehaviour
         }
 
         director = GetComponent<Animator>();
+
+        director.Play("IntroCinematic", 0);
     }
 
     private void Update()
     {
         if (acceptInput)
         {
-            if (Input.GetKeyDown(KeyCode.Q))
+            if (Input.GetKeyDown(KeyCode.Q)) // OPTION A
             {
                 Camille.GetComponent<Animator>().SetTrigger("OptionA");
                 director.Play("OptionA", 0);
                 acceptInput = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.W)) // OPTION B
+            {
+                director.Play("OptionB", 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E)) // OPTION C
+            {
+                director.Play("OptionC", 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.R)) // OPTION D
+            {
+                director.Play("OptionD", 0);
             }
         }
     }
@@ -40,5 +58,10 @@ public class DialogueSystem : MonoBehaviour
     {
         director.Play("ShowDialogueUI", 0);
         Debug.Log("Played!");
+    }
+
+    public void FinishedIntroCinematic()
+    {
+        PlayerCamera.SetActive(true);
     }
 }
