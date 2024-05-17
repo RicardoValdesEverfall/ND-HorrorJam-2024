af60809282a218d8ee0da1e3078fad9128fab4b8
RicardoValdesEverfall <72148793+RicardoValdesEverfall@users.noreply.github.com>
2024-05-17 13:32:47 -0400

    Day 1 cinematic

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -1,29 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueSystem : MonoBehaviour
 {
-    [SerializeField] GameObject Camille;
+    [SerializeField] Animator Camille;
     [SerializeField] GameObject PlayerCamera;
+    [SerializeField] Image SkipIcon;
+    [SerializeField] float timeToFirstInteraction;
+    [SerializeField] CanvasGroup[] choicesArray;
+    [SerializeField] TextMeshProUGUI[] textBoxes; //0 = dialogue, 1 = A, 2 = B, 3 = C, 4 = D
 
     private Animator director;
-    public CanvasGroup[] canvasGroup;
-    public float fadeDuration = 1f;
-    public float targetAlpha = 1f;
 
+    public string currentDialogueID;
     public bool acceptInput = false;
+    public bool skippable = false;
+
+    private float skipTimer = 0;
+    private bool day_one_started = false;
 
     private void Awake()
     {
         if (Camille == null)
         {
-            Camille = GameObject.FindGameObjectWithTag("Camille");
+            Camille = GameObject.FindGameObjectWithTag("Camille").GetComponent<Animator>();
         }
 
         director = GetComponent<Animator>();
-
         director.Play("IntroCinematic", 0);
+        skippable = true;
+
+        timeToFirstInteraction = Random.Range(20f, 120f);
     }
 
     private void Update()
@@ -52,16 +62,72 @@ public class DialogueSystem : MonoBehaviour
                 director.Play("OptionD", 0);
             }
         }
+
+
+    }
+
+    private void LateUpdate()
+    {
+        if (skippable)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                skipTimer += Time.deltaTime;
+                SkipIcon.fillAmount = skipTimer;
+
+                if (SkipIcon.fillAmount >= 0.99f)
+                {
+                    director.Play("IntroFinish", 0);
+                }
+            }
+
+            else
+            {
+                skipTimer = -Time.deltaTime * 1.4f;
+                SkipIcon.fillAmount = skipTimer;
+            }
+        }
+
+        if (!day_one_started)
+        {
+            timeToFirstInteraction -= Time.deltaTime;
+
+            if (timeToFirstInteraction <= 0)
+            {
+                day_one_started = true;
+                director.Play("Day1", 0);
+            }
+        }
+
+        else if (day_one_started)
+        {
+            DayOne_Main("main");
+        }
     }
 
     public void StartFadeIn()
     {
         director.Play("ShowDialogueUI", 0);
-        Debug.Log("Played!");
     }
 
     public void FinishedIntroCinematic()
     {
         PlayerCamera.SetActive(true);
+        skippable = false;
+    }
+
+    //#################################################### DAY SCENES SCRIPTING ###############################################
+
+    public void DayOne_Main(string dialogueChoiceID)
+    {
+        switch (dialogueChoiceID)
+        {
+            case "main":
+                choicesArray[2].alpha = 0; // OPTION C
+                choicesArray[3].alpha = 0; // OPTION D
+
+
+                break;
+        }
     }
 }
